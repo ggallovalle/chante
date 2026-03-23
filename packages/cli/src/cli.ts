c4492ce66f500a5fde996f93aaca621c96a56f39
Gerson Gallo <ggallovalle@gmail.com>
2026-03-23 11:04:07 -0600

    feat(cli): dry run flag

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,21 +1,23 @@
 import { Console, Effect } from "effect"
-import { Command, Flag } from "effect/unstable/cli"
+import { Command, Flag, GlobalFlag } from "effect/unstable/cli"
 import { NodeServices, NodeRuntime } from "@effect/platform-node"
 
 import * as env from "./env.js"
 import { parseFromCli, ChanteConfig } from "./config.js"
 import { renderSchemaError } from "./config-issue.js"
 
-
-const root = Command.make("chante").pipe(
-  Command.withDescription("A dotfiles manager")
-)
-
 const configFlag = Flag.file("config", { mustExist: true }).pipe(
   Flag.withAlias("c"),
   Flag.optional,
 )
 
+const dryRunFlag = Flag.boolean("dry-run").pipe(
+  Flag.withDescription("Do not apply the operations")
+)
+
+const root = Command.make("chante").pipe(
+  Command.withDescription("A dotfiles manager"),
+)
 
 const doctor = Command.make("doctor", { config: configFlag }, Effect.fn("doctor")(function*(cli) {
   const home = yield* env.HOME
@@ -35,14 +37,41 @@ const doctor = Command.make("doctor", { config: configFlag }, Effect.fn("doctor"
   Command.withDescription("Test if everything is set up correctly")
 )
 
-const up = Command.make("up", {}, Effect.fn("up")(function*() {
+const up = Command.make("up", { config: configFlag, dryRun: dryRunFlag }, Effect.fn("up")(function*(cli) {
   yield* Console.log("up")
+  const logLevel = yield* GlobalFlag.LogLevel
+  yield* Console.dir({
+    "dry-run": cli.dryRun,
+    "log-level": logLevel
+  }, { depth: null })
+
+  const config = yield* parseFromCli(cli.config)
+  yield* Console.info("config file")
+  const configJson = yield* ChanteConfig.encodeUnknownAsJson(config.data)
+  yield* Console.dir(configJson, { depth: null })
 })).pipe(
   Command.withDescription("Apply the configuration, linking, copying and templating dotfiles")
 )
 
-const down = Command.make("down", {}, Effect.fn("down")(function*() {
+const down = Command.make("down", { config: configFlag, dryRun: dryRunFlag }, Effect.fn("down")(function*(cli) {
   yield* Console.log("down")
+  const logLevel = yield* GlobalFlag.LogLevel
+  yield* Console.dir({
+    "dry-run": cli.dryRun,
+    "log-level": logLevel
+  }, { depth: null })
+
+  yield* Effect.logTrace("effect log trace")
+  yield* Effect.logDebug("effect log debug")
+  yield* Effect.logInfo("effect log info")
+  yield* Effect.logWarning("effect log warning")
+  yield* Effect.logError("effect log error")
+  yield* Effect.logFatal("effect log fatal")
+
+  const config = yield* parseFromCli(cli.config)
+  yield* Console.info("config file")
+  const configJson = yield* ChanteConfig.encodeUnknownAsJson(config.data)
+  yield* Console.dir(configJson, { depth: null })
 }))
   .pipe(
     Command.withDescription("Undo the configuration, removing links and files created by chante")
