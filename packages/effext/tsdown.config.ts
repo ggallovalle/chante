fa8a20441984fd7f974eb6d36f2d98fcb3a03675
Gerson Gallo <ggallovalle@gmail.com>
2026-04-04 03:30:58 -0600

    chore(effext): almost ready for publishing

--- packages/effext/tsdown.config.ts
+++ packages/effext/tsdown.config.ts
@@ -2,16 +2,26 @@ import { defineConfig } from "tsdown"
 
 export default defineConfig([
   {
-    entry: {
-      "testing/vitest": "src/testing/vitest.ts",
-      uwu: "src/uwu.ts",
-      "uwu/bun": "src/uwu/bun.ts",
-      "uwu/node": "src/uwu/node.ts",
-      miette: "src/miette.ts",
-      kdl: "src/kdl.ts",
-    },
+    entry: [
+      "src/testing/vitest.ts",
+      "src/uwu.ts",
+      "src/uwu/bun.ts",
+      "src/uwu/node.ts",
+      "src/miette.ts",
+      "src/kdl.ts",
+    ],
     format: "esm",
-    dts: true,
+    fixedExtension: true,
+    exports: {
+      enabled: true,
+      devExports: true,
+      packageJson: false,
+    },
+    publint: true,
+    dts: {
+      enabled: true,
+      tsgo: true,
+    },
     clean: true,
   },
 ])
